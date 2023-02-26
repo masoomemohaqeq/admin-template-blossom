f933f920dfdc2cad942fccd2f24b6d970ce95b3a
Mohaqeq_m <masoome.mohaqeq@outlook.com>
2023-02-26 21:38:18 +0330

    add notif and profile menu

---
+++ src/js/global.js
@@ -0,0 +1,23 @@
+// Toggle Floating Menu
+document.body.addEventListener("click", function (event) {
+  let isClickInside = [
+    ...document.querySelectorAll(".toggle-floating-btn"),
+  ].some((el) => el.contains(event.target));
+
+  let allChkTogglers = document.querySelectorAll(".toggle-floating-menu");
+  if (isClickInside) {
+    let specifiedInput = event.target
+      .closest(".floating-menu-container")
+      .querySelector(".toggle-floating-menu");
+
+    allChkTogglers.forEach((el) => {
+      if (el != specifiedInput) el.checked = false;
+    });
+
+    specifiedInput.checked = !specifiedInput.checked;
+  } else {
+    allChkTogglers.forEach((el) => {
+      el.checked = false;
+    });
+  }
+});
