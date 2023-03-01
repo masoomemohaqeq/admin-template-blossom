e69844459fef6ccbe4b8cfdfc8850381eaf2e696
Mohaqeq_m <masoome.mohaqeq@outlook.com>
2023-03-01 20:25:54 +0330

    add eye slash showpassword

--- src/js/login.js
+++ src/js/login.js
@@ -1,8 +1,17 @@
-document.getElementById("showPassword").addEventListener("click", () => {
-  var passwordElement = document.getElementById("password");
-  if (passwordElement.type == "password") {
-    passwordElement.type = "text";
-  } else {
-    passwordElement.type = "password";
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  const showPasswordBtn = document.getElementById("showPassword");
+  showPasswordBtn.addEventListener("click", () => {
+    var passwordElement = document.getElementById("password");
+    if (passwordElement.type == "password") {
+      passwordElement.type = "text";
+
+      showPasswordBtn.classList.add("fa-eye-slash");
+      showPasswordBtn.classList.remove("fa-eye");
+    } else {
+      passwordElement.type = "password";
+
+      showPasswordBtn.classList.remove("fa-eye-slash");
+      showPasswordBtn.classList.add("fa-eye");
+    }
+  });
 });
